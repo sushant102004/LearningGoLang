fe490ffb6783c5623ca52e4acfd04ead527ed801
sushant102004 <sushant.dhiman9812@gmail.com>
2023-03-23 22:11:51 +0530

    Variadics

--- 014-Variadics/variadics.go
+++ 014-Variadics/variadics.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+// When we don't know exact number of arguments then we can use
+// varidaics.
+func addNumbers(nums ...int) int {
+	sum := 0
 
+	for _, num := range nums {
+		sum += num
+	}
+
+	return sum
+}
+
+func main() {
+	arrOne := []int{1, 2, 3}
+	arrTwo := []int{4, 5, 6}
+
+	all := append(arrOne, arrTwo...)
+	println(addNumbers(all...))
 }
