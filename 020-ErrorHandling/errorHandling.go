c04c17aab6f2aa2dba74a155603d32f215c05ba5
sushant102004 <sushant.dhiman9812@gmail.com>
2023-03-26 11:58:53 +0530

    🐛 fix(errorHandling.go): change file path to absolute path ✨ feat(errorHandling.go): add divide function to handle division by zero errors

--- 020-ErrorHandling/errorHandling.go
+++ 020-ErrorHandling/errorHandling.go
@@ -1,16 +1,33 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 )
 
+func divide(a, b float64) (float64, error) {
+	if b == 0 {
+		return float64(0), errors.New("can't divide by zero")
+	} else {
+		return float64(a) / float64(b), nil
+	}
+}
+
 func main() {
-	file, err := os.ReadFile("temp.go")
+	file, err := os.ReadFile("/mnt/07df2e6f-7d6b-4909-9ed8-b51af0bfa52c/GoLang/LearningGoLang/020-ErrorHandling/data.txt")
 
 	if err != nil {
 		fmt.Println("Error Reading File: ", err)
 	} else {
 		fmt.Println(string(file))
+	}
+
+	ans, error := divide(3, 12)
+
+	if error != nil {
+		fmt.Println(error)
+	} else {
+		fmt.Println(ans)
 	}
 }
