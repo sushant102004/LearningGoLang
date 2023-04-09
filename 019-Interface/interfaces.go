06bd9a97328dca38185668b15be6713816e0300c
sushant102004 <sushant.dhiman9812@gmail.com>
2023-04-09 21:45:50 +0530

    feat (interfaces.go) - Interface based salary calculator.

--- 019-Interface/interfaces.go
+++ 019-Interface/interfaces.go
@@ -23,13 +23,69 @@ func (rectangle Rectangle) area() float64 {
 	return 2 * (rectangle.width + rectangle.height)
 }
 
-func main() {
-	circle := Circle{9.9}
-	rectangle := Rectangle{3, 4}
+// Salary Calculation using interface.
+type SalaryCalculator interface {
+	CalculateSalary() int
+}
 
-	shapes := []Shapes{circle, rectangle}
+type Permanent struct {
+	empID   string
+	basePay int
+	pf      int
+}
 
-	for _, shape := range shapes {
-		fmt.Println(shape.area())
+type Contract struct {
+	empID   string
+	basePay int
+}
+
+func (p Permanent) CalculateSalary() int {
+	return p.basePay + p.pf
+}
+
+func (c Contract) CalculateSalary() int {
+	return c.basePay
+}
+
+func totalSalary(employees []SalaryCalculator) int {
+	totalExpense := 0
+
+	for _, person := range employees {
+		totalExpense = totalExpense + person.CalculateSalary()
 	}
+
+	return totalExpense
+}
+
+func main() {
+	// circle := Circle{9.9}
+	// rectangle := Rectangle{3, 4}
+
+	// shapes := []Shapes{circle, rectangle}
+
+	// for _, shape := range shapes {
+	// 	fmt.Println(shape.area())
+	// }
+
+	john := Permanent{
+		empID:   "John_SDE",
+		basePay: 60000,
+		pf:      3000,
+	}
+
+	ella := Permanent{
+		empID:   "Ella_SDE",
+		basePay: 7000,
+		pf:      3500,
+	}
+
+	ramesh := Contract{
+		empID:   "Ramesh_Intern",
+		basePay: 10000,
+	}
+
+	employees := []SalaryCalculator{john, ella, ramesh}
+
+	total := totalSalary(employees)
+	fmt.Println(total)
 }
